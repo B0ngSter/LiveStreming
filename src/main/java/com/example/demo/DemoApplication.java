276aa56e502ddbd725f586782ea993d30c1146bf
Bongster <chutiyapakayamrahe@gmail.com>
2025-08-31 01:45:35 +0530

    restructure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.demo.repository")
+@ComponentScan(basePackages = {"com.example.demo.controller", "com.example.demo.model"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
